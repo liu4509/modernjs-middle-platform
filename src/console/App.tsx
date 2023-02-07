bff7e78de699fb9dd024c7eac18521d3995310cd
zhibin <2608606700@qq.com>
2023-02-07 23:58:38 +0800

    创建入口

---
+++ src/console/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+const App = () => (
+  <div>
+    <div>This is a console</div>
+  </div>
+);
+
+export default App;
---
+++ src/middle-platform/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+const App = () => (
+  <div>
+    <div>This is a landing page</div>
+    <a href="/console">Go to console</a>
+  </div>
+);
+
+export default App;
