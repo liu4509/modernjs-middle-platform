4239dc1513648f3e1bb8a5ace6a866e27336b140
zhibin <2608606700@qq.com>
2023-02-08 21:58:05 +0800

    代码分片

--- src/console/App.tsx
+++ src/console/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import loadable from '@modern-js/runtime/loadable';
 import { Route, Switch, Link } from '@modern-js/runtime/router';
-import Dashboard from './dashboard';
-import TableList from './tableList';
 import './App.css';
+
+// import Dashboard from './dashboard';
+// import TableList from './tableList';
+// 代码分片 通过 loadable 按需加载对应路由所需要的组件代码
+const Dashboard = loadable(() => import('./dashboard'));
+const TableList = loadable(() => import('./tableList'));
+// 当访问 /console 路由时，会加载 src_console_dashboard_index_tsx.js 这个文件；当访问 /console/table 路由时，会加载 src_console_tableList_index_tsx.js 这个文件
 
 const App: React.FC = () => {
   return (
     <div>
-      <a href=""></a>
+      <a href="/">Go to Index</a>
       <div>
         <Link to="/">Dashboard | </Link>
         <Link to="/table">TableList</Link>
